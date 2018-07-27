c8ddf638c7302f31ce44beb65bdc26b983b9de77
Antti Kaihola <antti+jupyter_abc@kaihola.fi>
2018-07-28 01:00:40 +0300

    Install the abcjs library so Jupyter will serve it

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup
 
-setup()
+setup(
+    data_files=[('share/jupyter/nbextensions/jupyter-abc',
+                 ['jupyter_abc/static/abcjs_basic_5.1.2-min.js'])]
+    # The rest of the setuptools configuration comes from `setup.cfg`. The
+    # `data_files` argument is here since it's not yet supported in
+    # `setup.cfg`.
+)
